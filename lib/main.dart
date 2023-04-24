dd8a13a05093f3467c8b6cb52f5c88707020b7c3
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-04-24 16:37:00 -0600

    routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_segundo/product.dart';
+import 'package:flutter_segundo/purchase.dart';
 import 'package:flutter_segundo/register.dart';
+import 'package:flutter_segundo/sales.dart';
 import 'package:flutter_segundo/screen_login.dart';
+import 'package:flutter_segundo/screen_menu.dart';
+import 'package:flutter_segundo/user.dart';
 import 'package:flutter_segundo/widgets/inputField.dart';
 
 void main() {
@@ -19,6 +24,14 @@ class MainApp extends StatelessWidget {
           child: ScreenLogin(),
         ),
       ),
+      routes: {
+        'login': ( _ ) => ScreenLogin(),
+        'sales': ( _ ) => SalesScreen(),
+        'menu': ( _ ) => ScreenMenu(),
+        'products': ( _ ) => ProductScreen(),
+        'purchase': ( _ ) => PurchaseScreen(),
+        'user': ( _ ) => UserScreen(),
+      },
     );
   }
 }
