16135fd6f011561cbc4d519c275a342857d79045
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-04-27 14:26:35 -0600

    receives the type of keybord

--- lib/widgets/inputField.dart
+++ lib/widgets/inputField.dart
@@ -3,22 +3,41 @@
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField({super.key, required this.text});
+  const InputField({super.key, required this.text, this.type});
 
   final text;
+  final type;
+  
   @override
   Widget build(BuildContext context) {
+    var isObscure = false;
+    TextInputType inputType = TextInputType.text;
+    // verifies the type of keybord which will be displayes
+      if ( type == "pass"){
+         isObscure = true;
+         inputType = TextInputType.visiblePassword;
+      }
+      if (type == "email") {
+        inputType = TextInputType.emailAddress;
+      }
+      if (type == "date") {
+        inputType = TextInputType.datetime;
+      }
+      if (type == "number") {
+        inputType = TextInputType.number;
+      }
     return Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextFormField(
-            obscureText: false,
-            keyboardType: TextInputType.text,
+            obscureText: isObscure,
+            keyboardType: inputType,
             decoration: InputDecoration(
               border: const OutlineInputBorder(),
               labelText: text + ":",
             ),
+            // validates if the value is null or empty
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Please enter some text';
@@ -31,4 +50,25 @@ class InputField extends StatelessWidget {
       ),
     );
   }
+  
+    List<dynamic> newType() {
+    var isObscure = false;
+    TextInputType inputType = TextInputType.text;
+          if ( type == "pass"){
+         isObscure = true;
+         inputType = TextInputType.visiblePassword;
+      }
+      if (type == "email") {
+        inputType = TextInputType.emailAddress;
+      }
+      if (type == "date") {
+        inputType = TextInputType.datetime;
+      }
+      if (type == "number") {
+        inputType = TextInputType.number;
+      }
+      return [isObscure, inputType];
+      //return inputType;
+  }
+  
 }
